61adf4904b9ef3ddcc4f921b2d3b5d06e273bb8c
Nithin Kumar <n.kumar4@elsevier.com>
2024-01-16 12:01:18 +0530

    added navbar scrolled animation and effects

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-scroll";
 import "./Navbar.css";
 import { Fade } from "react-awesome-reveal";
 const Navbar = () => {
   const [open, setOpen] = useState(false);
+  const [Bg, setBg] = useState(false);
 
   const handleNavbarClick = () => {
     setOpen((previous) => !previous);
@@ -13,9 +14,15 @@ const Navbar = () => {
     setOpen(false);
   };
 
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      return window.scrollY > 50 || open ? setBg(true) : setBg(false);
+    });
+  });
+
   return (
     <>
-      <div id="navbar" className="navbar scrolled">
+      <div id="navbar" className={`navbar ${Bg? 'scrolled' :''}`}>
         <nav className={open ? "item-container-active" : "item-container"}>
         <Fade direction="down" delay={100} cascade duration={250} triggerOnce="true">
           <ul>
